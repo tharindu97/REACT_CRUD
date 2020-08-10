f747b8169b9277e4e212b3e28ab8ed9cc3fdac9d
tharindu97 <tharindukavishna@gmail.com>
2020-08-10 16:26:20 +0530

    strated_project

---
+++ react_crud/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>panama</h2>
+    </div>
+  );
+}
+
+export default App;
